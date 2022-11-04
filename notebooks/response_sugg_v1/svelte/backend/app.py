b3545a94ea0e01eb122908cf1f2ca4fef304a39f
Petr Glivicky <petr.glivicky@sbgservices.eu>
2022-11-04 11:05:42 +0100

    Implement probability based next word suggestion

--- notebooks/response_sugg_v1/svelte/backend/app.py
+++ notebooks/response_sugg_v1/svelte/backend/app.py
@@ -1,41 +1,8 @@
 from fastapi import FastAPI
-from flask import jsonify
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
-import set_cwd
-import re
 
-# data = pd.read_csv("data_dump/response_sugg/data_v1.csv")
-data = pd.read_csv(
-    "C:/Users/feress/Documents/myprojects/Chat-response-suggestions/data_dump/response_sugg/data_v1.csv"
-)
-data = data.dropna()
-agent_msgs = data[data.user_id > 0]
-
-# agent_unique = agent_msgs.value_counts().reset_index()
-agent_unique = agent_msgs.msg.unique()
-
-agent_msgs = []
-for msg in agent_unique:
-    splited = re.split(". |, |;|\n|\? |\! ", msg)
-    agent_msgs.extend(splited)
-
-# languages = [
-#     "C++",
-#     "Python",
-#     "PHP",
-#     "Java",
-#     "C",
-#     "Ruby",
-#     "R",
-#     "C#",
-#     "Dart",
-#     "Fortran",
-#     "Pascal",
-#     "Javascript",
-# ]
-
+from src.autocomplete_preprocess import get_agent_msgs, get_probable_continuations
 
 app = FastAPI()
 
@@ -55,44 +22,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 class Item(BaseModel):
     text: str
 
-
-# @app.post("/post")
-# async def root(data: Item):
-#     # print(data.text)
-#     # filtered_dict = [v for v in languages if data.text in v]
-#     # result = [{"sugg": x} for x in filtered_dict]
-
-#     return data
-
-
 @app.post("/")
 async def root(data: Item):
-    print(data.text)
-    prefix = data.text.lower().split()
-
-    filtered_dict = [
-        v for v in agent_msgs if data.text.lower() == v.lower()[: len(data.text)]
-    ]
-    # test = True
-    # for x in prefix:
-    #     if x in v
-    # filtered_dict = [
-    #     v for v in agent_unique if all([item in v.lower() for item in prefix])
-    # ]
-    # filtered_dict = [" ".join(x.split()[:6]) for x in filtered_dict]
-    result = [{"sugg": x} for x in filtered_dict]
-
-    return result
-
-
-# @app.get("/")
-# async def root():
-#     # print(data.text)
-#     filtered_dict = [v for v in languages if "p" in v]
-#     result = [{"sugg": x} for x in filtered_dict]
-#     print("end succ")
-#     return result
+    prefix = data.text.lower()
+    agent_msgs = get_agent_msgs()
+    suggestions = get_probable_continuations(prefix, agent_msgs)
+    return [{"sugg": sugg, "prob": row["prob"]} for sugg, row in suggestions.iterrows()]
--- notebooks/response_sugg_v1/svelte/myapp/src/App.svelte
+++ notebooks/response_sugg_v1/svelte/myapp/src/App.svelte
@@ -1,33 +1,10 @@
 <script>
   import AutoComplete from "simple-svelte-autocomplete";
-  let selectedCountry;
-
-  // async function f(keyword) {
-  //   const myImage = document.querySelector(".my-image");
-  //   await fetch(
-  //     "https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg"
-  //   )
-  //     .then((response) => {
-  //       console.log(response.bodyUsed);
-  //       const res = response.blob();
-  //       console.log(response.bodyUsed);
-  //       return res;
-  //     })
-  //     .then((response) => {
-  //       const objectURL = URL.createObjectURL(response);
-  //       // myImage.src = objectURL;
-  //     });
-  // }
+  let selectedSugg;
 
   async function get_suggestions_list(keyword) {
-    // let x = JSON.stringify({ text: "P" });
     let data = { text: keyword };
 
-    // const url2 =
-    //   "https://restcountries.com/v2/name/" +
-    //   encodeURIComponent(keyword) +
-    //   "?fields=name;alpha2Code";
-    // const url = "http://127.0.0.1:8000?text=" + encodeURIComponent(keyword);
     const url = "http://127.0.0.1:8000/";
     const response = await fetch(url, {
       method: "POST",
@@ -39,13 +16,20 @@
     });
     return response.json();
   }
+
+//  function onKeyDown(e) {
+//    if (e.key == 'Tab') {
+//
+//    }
+//  }
 </script>
 
 <AutoComplete
   searchFunction={get_suggestions_list}
-  bind:selectedItem={selectedCountry}
-  labelFieldName="sugg"
+  bind:selectedItem={selectedSugg}
+  keywordsFieldName="sugg"
+  labelFunction={suggestion => `${suggestion.sugg} (${(100*suggestion.prob).toFixed(2)}%)`}
   maxItemsToShowInList={10}
-  delay={200}
+  delay={300}
   localFiltering={false}
 />
