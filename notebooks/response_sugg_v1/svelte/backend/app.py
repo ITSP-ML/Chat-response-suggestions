a2183b83fd27734f75ee58f3a8474798faf30981
Petr Glivicky <petr.glivicky@sbgservices.eu>
2022-11-04 15:34:16 +0100

    Optimize speed of API calls by preloading agent messages

--- notebooks/response_sugg_v1/svelte/backend/app.py
+++ notebooks/response_sugg_v1/svelte/backend/app.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from src.autocomplete_preprocess import get_agent_msgs, get_probable_continuations
 
+agent_msgs = get_agent_msgs()
+
 app = FastAPI()
 
 origins = ["http://localhost:8000"]
@@ -19,10 +21,10 @@ app.add_middleware(
 class Item(BaseModel):
     text: str
 
+
 @app.post("/")
 async def root(data: Item):
     prefix = data.text
-    agent_msgs = get_agent_msgs()
     suggestions = get_probable_continuations(prefix, agent_msgs)
     return [{"prefix": prefix, "sugg": sugg, "prob": row["prob"]} for sugg,
                                                                  row in suggestions.iterrows()]
