f907f9b711d474d78ccc63d185324e8c296ae040
feres sakouhi <11feressakouhi11@gmail.com>
2023-01-30 15:06:24 +0100

    Add generate embeddings script for sementic search

---
+++ src/src_models/sementic_search/generate_embeddings.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pickle
+from sentence_transformers import SentenceTransformer
+
+from src.preprocess.autocomplete_preprocess import get_agent_msgs
+def generate_embeddings(model_name, data_path, save_path):
+    """
+    model_name: the name of model used for generating embeddings
+    data_path: data_path for data to be encoded
+    """
+    # load model
+    model = SentenceTransformer(model_name)
+    # read data
+    data = get_agent_msgs(data_path)
+    # generate embeddings
+    encoded_data = model.encode(data.processed_msg.tolist())
+    encoded_data = np.asarray(encoded_data.astype('float32'))
+    # save embeddings
+    with open(save_path, 'wb') as pkl:
+        pickle.dump(encoded_data, pkl)
+    return encoded_data
