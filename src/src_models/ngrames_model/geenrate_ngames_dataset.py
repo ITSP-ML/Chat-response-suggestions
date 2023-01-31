d48a31aa3b889e59678ce4ce701965115c38909a
feres sakouhi <11feressakouhi11@gmail.com>
2023-01-31 15:16:21 +0100

    improve the ngram data generation to contain the level of each ngram

--- src/src_models/ngrames_model/geenrate_ngames_dataset.py
+++ src/src_models/ngrames_model/geenrate_ngames_dataset.py
@@ -1,37 +1,43 @@
 import pandas as pd
 from src.preprocess.autocomplete_preprocess import get_agent_msgs
+
 def get_ngrames(sentance):
     """
-    function that get all ngrames from a sentance
+    function that get all ngrames and their status from a senatance
+    level of an ngrame will be use it to filter the Trie
     """
-    final_output =[]
+    ngram_level_dict ={}
     words = sentance.split()
     tmp = ''
-    for word in words:
+    for i, word in enumerate(words):
         tmp = tmp + word + ' '
-        final_output.append(tmp[:-1])
-    return (final_output)
+        ngram_level_dict[tmp[:-1]] = i # this will indicate the level of ngrame (the level increase with the number of words) | also the same as the number of words 
 
-def generate_ngrams_dataset(old_dataset, path = None):
+    # ngram_level_dict[sentance] = -1 # indicate this is the original sentance
+    return ngram_level_dict
+
+def generate_ngrams_dataset(old_dataset, path= None):
     """
     get all ngrames from sentances
     """
     output = {}
+    final_ngrame_dict = {}
     for i, row in old_dataset.iterrows():
         msg, freq, l= row
         ngrames = get_ngrames(msg)
-        for x in ngrames:
-            if x not in list(output.keys()):
-                output[x] = freq
+        for msg, level in ngrames.items():
+            final_ngrame_dict[msg] = level # add or update the level of the ngram
+
+            if msg not in list(output.keys()):
+                output[msg] = freq
             else:
-                output[x] += freq
-    final_dict = {"processed_msg": list(output.keys()), 'freq':  list(output.values())}
-    final_data = pd.DataFrame(final_dict)
+                output[msg] += freq
+    final_result = {"processed_msg": list(output.keys()), 'freq':  list(output.values()), "ngram_level": list(final_ngrame_dict.values())}
+    final_data = pd.DataFrame(final_result)
 
     # save this dataset
     if path is not None:
         final_data.to_csv(path, index= False)
-
 
     return final_data
 
@@ -40,6 +46,6 @@ def generate_ngrams_dataset(old_dataset, path = None):
 if __name__ == "__main__":
 
     old_data_path = 'data/prod_v1/agent_data.csv'
-    save_path = "data/prod_v1/agent_data_2.csv"
+    save_path = "data/prod_v1/agent_data_3.csv"
     old_data = get_agent_msgs(old_data_path)
     generate_ngrams_dataset(old_data, save_path)
