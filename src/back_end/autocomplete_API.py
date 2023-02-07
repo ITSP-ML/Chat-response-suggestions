5d95e8f046390ad9aa01760bb631ee43173609e9
feres sakouhi <11feressakouhi11@gmail.com>
2023-02-07 08:28:40 +0100

    Separate sementic search and autocomplete into 2 APIs

--- src/back_end/autocomplete_API.py
+++ src/back_end/autocomplete_API.py
@@ -2,13 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import set_cwd
-import sys
-import pandas as pd
 
 
 
 from src.preprocess.autocomplete_preprocess import get_agent_msgs, preprocess_msg, change_recursion_limit
-from src.src_models.sementic_search.model import get_sementic_match
 from src.src_models.ngrames_model.model import build_trie, get_words_match
 
 
@@ -43,9 +40,7 @@ class Item(BaseModel):
 
 # read agent data
 dataset_path = 'data/prod_v1/agent_data_3.csv'
-dataset_path2 = 'data/prod_v1/agent_data.csv'
 dataset = get_agent_msgs(dataset_path)
-dataset_2 = get_agent_msgs(dataset_path2)
 
 # change recursion limit
 limit = 10000000
@@ -61,7 +56,6 @@ t = build_trie(dataset, validation_threshold, max_number_of_suggestions = max_nb
 model_name = 'distilbert-base-nli-stsb-mean-tokens'
 embbeding_path = "data/prod_v1/doc_embedding.pickle"
 print('all_done')
-min_prob = 0
 
 
 
@@ -74,12 +68,6 @@ async def root(data: Item):
     # preprocess prefix 
     pre_prefix = preprocess_msg(prefix)
     words_match = get_words_match(pre_prefix, t)
-    if len(words_match) < max_nb_suggs:
-        sementic_match = get_sementic_match(pre_prefix, dataset_2, model_name, embbeding_path, top_k_hits = 10,
-                                             min_prob = min_prob,  max_number_of_words = 3)[:(max_nb_suggs - len(words_match))]
-        return words_match +sementic_match
-        # return sementic_match
-    else:
-        return words_match
+    return words_match
     # return get_sementic_match(pre_prefix, dataset_2, model_name, embbeding_path, top_k_hits = 10,
     #                                          min_prob = min_prob,  max_number_of_words = 3)
