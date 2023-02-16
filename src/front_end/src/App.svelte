1518c937c2b741698efbd9c32958d00945752f32
feres sakouhi <11feressakouhi11@gmail.com>
2023-02-16 02:59:36 +0100

    Update the front end

--- src/front_end/src/App.svelte
+++ src/front_end/src/App.svelte
@@ -4,7 +4,8 @@
   async function get_suggestions_list(keyword) {
     // let x = JSON.stringify({ text: "P" });
     let data = { text: keyword };
-    const url = "http://127.0.0.1:8010/";
+    const url = "http://172.26.103.221:5010/autocomplete/search"; // for AutoComplete
+    // const url = "http://172.26.103.221:5010/semantic_search/search"; // for semantic search
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -13,7 +14,8 @@
       },
       body: JSON.stringify(data),
     });
-    return response.json();
+    const json = await response.json()
+    return json.suggestions.map(x => ({"sugg": x})); 
   }
 </script>
 
@@ -26,23 +28,3 @@
   localFiltering={false}
   lowercaseKeywords={true}
 />
-<!-- <script>
-  import AutoComplete from "simple-svelte-autocomplete";
-  let selectedCountry;
-  async function searchCountry(keyword) {
-    const url =
-      "https://restcountries.com/v2/name/" +
-      encodeURIComponent(keyword) +
-      "?fields=name;alpha2Code";
-    const response = await fetch(url);
-    return await response.json();
-  }
-</script>
-<AutoComplete
-  searchFunction={searchCountry}
-  bind:selectedItem={selectedCountry}
-  labelFieldName="name"
-  maxItemsToShowInList={10}
-  delay={200}
-  localFiltering={false}
-/> -->
